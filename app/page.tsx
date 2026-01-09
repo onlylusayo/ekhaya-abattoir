eed09f9f4fa7df148b0e1457fd8408c877a9d7e1
Timothy Msiska <msiskazikani@gmail.com>
2026-01-09 09:46:57 +0200

    removed why us page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 import HeroSection from "./section/landing-page/page";
-import AboutSection from "./section/about/page";
-import HowItWorksSection from "./section/how-it-works/page";
-import PricingSection from "./section/pricing/page";
-import { WhyUsSection } from "./section/why-us/page";
+// import AboutSection from "./section/about/page";
+// import HowItWorksSection from "./section/how-it-works/page";
+// import PricingSection from "./section/pricing/page";
+// import { WhyUsSection } from "./section/why-us/page";
 
 export default function Home() {
   return (
